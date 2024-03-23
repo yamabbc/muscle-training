2f2af49a38f92acdef2786829d476340524ee5ca
yamabbc <awesome01280@gmail.com>
2024-03-23 12:40:26 +0900

    deploy to heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
