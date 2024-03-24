6c543fd5a1ecece47fa146ab04f9d613ba2bef33
yamabbc <awesome01280@gmail.com>
2024-03-25 02:45:50 +0900

    画像変更

--- resources/views/explanations/select.blade.php
+++ resources/views/explanations/select.blade.php
@@ -16,7 +16,7 @@
                     <h3>部位を選択してください</h3>
                     <div class="row">
                       <div class="col-6 text-center">
-                        <a href="{{ route('explanations.show',[$trainings[0]])}}"><button type=="button" class="rounded"><img src="{{asset('storage/表紙腕解説.png')}}"height="auto" width="150px"></button><br><p>腕解説</p></a><br>
+                        <a href="{{ route('explanations.show',[0])}}"><button type=="button" class="rounded"><img src="{{asset('storage/表紙腕解説.png')}}"height="auto" width="150px"></button><br><p>腕解説</p></a><br>
                       </div>
                       <div class="col-6 text-center">
                         <a href="{{ route('explanations.show',[$trainings[1]])}}"><button type=="button" class="rounded"><img src="{{asset('storage/表紙肩解説.png')}}"height="auto" width="150px"></button><br><p>肩解説</p></a><br>
